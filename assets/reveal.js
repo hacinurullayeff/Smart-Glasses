060449759906bb9829e8ccf654978f6096039c5a
hacinurullayeff <hacinurullayeff@gmail.com>
2022-02-22 18:23:22 +0400

    mu-posh

---
+++ assets/reveal.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal('.reveal1', { delay: 250 });
+ScrollReveal().reveal('.reveal2', { delay: 500 });
+ScrollReveal().reveal('.reveal3', { delay: 750 });
+ScrollReveal().reveal('.reveal4', { delay: 1000 });
+ScrollReveal().reveal('.reveal5', { delay: 1250 });
+ScrollReveal().reveal('.reveal6', { delay: 1500 });
+
+
+window.sr=ScrollReveal();
+
+sr.reveal('.left-to-right',{origin:'left',duration:1000,distance:'25rem',delay:300})
+sr.reveal('.right-to-left',{origin:'right',duration:1000,distance:'25rem',delay:600})
+sr.reveal('.top-to-bottom',{origin:'top',duration:1000,distance:'25rem',delay:600})
